000a2d405325a83b48bfb4f03996d5cd4c928e25
david <david.brandstoetter@student.tugraz.at>
2021-03-16 16:07:39 +0100

    read and compute x hat

--- main_lab1.py
+++ main_lab1.py
@@ -6,3 +6,7 @@ Created on Tue Mar 16 15:19:06 2021
 @author: david
 """
 
+import numpy as np
+n = np.load("normals_2008-05.rightHandSide.npy")
+N = np.load("normals_2008-05.npy")
+x_hat = np.linalg.inv(N)@n
